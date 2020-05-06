dd5c0cf1d07f2b1b8e72b28e2ebf1a46bc8be159
Andy Truong <thehongtt@gmail.com>
2020-05-06 22:41:41 +1000

    Add NilPointerError

--- pkg/util/error.go
+++ pkg/util/error.go
@@ -1,6 +1,14 @@
 package util
 
-type Error struct {
-	Code   *ErrorCode `json:"code"`
-	Fields []string   `json:"fields"`
-}
+type (
+	Error struct {
+		Code   *ErrorCode `json:"code"`
+		Fields []string   `json:"fields"`
+	}
+
+	Err string
+)
+
+func (e Err) Error() string { return string(e) }
+
+const NilPointerError = Err("nil pointer error")
