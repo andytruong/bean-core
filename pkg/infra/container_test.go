0c14f3785d6ee0c52e078828c6f5c0ba50af98d8
Andy Truong <thehongtt@gmail.com>
2020-06-06 21:19:13 +1000

    Fix test error on Test_Request_JWT()

--- pkg/infra/container_test.go
+++ pkg/infra/container_test.go
@@ -42,6 +42,6 @@ func Test_Request_JWT(t *testing.T) {
 	{
 		r.Header.Add("Authorization", "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIwMUVBMzc4RTlFU01STjJaWk5OMURZN04xOCIsImV4cCI6MTU5MTQ0MDgyNywianRpIjoiMDFFQTNROE1WTThDWkY3WFRZTllQWE1OWlYiLCJpYXQiOjE1OTE0NDA1MjcsImlzcyI6ImFjY2VzcyIsInN1YiI6IjAxRUEzNzg0VEU5NEtLVkNUS1I2VkoyR1dDIn0.BO36niHSF3Svzg4oIQ7A8bEScQYrWbvIlBZ5ExakOoEd5CZGuRQbAQRcF0skiqQz8cdVHb3pkcm7LUkJ7zi7WXKdnhd7M-NceGmwQ0XJ9NE9eZvYP5swFxjxVVYxTxjfWQp-5buP3UXkLeL2UhUINsFYJpxQUWKxLG-vdkCzRkcNH8VBkB-XTAfg7lX4ESGObVo-AxyxzrSPj2TWGNHnd5WrB6nFmz_up6vJ89aiizDI7zVnku-lJzPW0AJmiBFAyTD6y9WN0uKdBrGEzJ3wfW8EIadHgqcP7RmCF-XVD4ILIU3nwg-DQ8SQgjBpgcRyPTAawkOsIR6ubfQRS_J21A")
 		err := container.BeforeServeHTTP(r)
-		ass.NoError(err)
+		ass.Contains(err.Error(), "token is expired by")
 	}
 }
