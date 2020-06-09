10682f58fed152da8f370b55bf190601a8303007
Andy Truong <thehongtt@gmail.com>
2020-06-09 19:58:59 +1000

    #config: Stub for API.

---
+++ pkg/config/model/dto/create.go
@@ -0,0 +1,20 @@
+package dto
+
+import (
+	"bean/pkg/config/model"
+	"bean/pkg/util"
+	"bean/pkg/util/api"
+)
+
+type BucketCreateInput struct {
+	HostId      string          `json:"hostId"`
+	Slug        *string         `json:"slug"`
+	Title       *string         `json:"title"`
+	Description *string         `json:"description"`
+	Access      *api.AccessMode `json:"access"`
+}
+
+type BucketMutationOutcome struct {
+	Errors []util.Error        `json:"errors"`
+	Bucket *model.ConfigBucket `json:"bucket"`
+}
