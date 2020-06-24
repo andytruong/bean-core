1d9a8b84fdc941f09c6bccef6a576ed2ee403233
Andy Truong <thehongtt@gmail.com>
2020-06-25 08:09:19 +1000

    #integration/s3: Add some more progress.

---
+++ pkg/integration/s3/model/migration/001_s3.sqlite3.sql
@@ -0,0 +1,57 @@
+CREATE TABLE s3_application
+(
+    id             character varying(26) NOT NULL PRIMARY KEY,
+    version        character varying(26) NOT NULL UNIQUE,
+    slug           character varying(64) NOT NULL UNIQUE,
+    is_active      boolean               NOT NULL,
+    created_at     timestamp             NOT NULL,
+    updated_at     timestamp             NOT NULL,
+    deleted_at     timestamp,
+    credentials_id character varying(26) NOT NULL
+);
+
+CREATE TABLE s3_credentials
+(
+    id                 character varying(26)  NOT NULL PRIMARY KEY,
+    version            character varying(26)  NOT NULL UNIQUE,
+    endpoint           character varying(256) NOT NULL,
+    encrypted_key_pair character varying(256) NOT NULL,
+    is_secure          boolean                NOT NULL
+);
+
+CREATE TABLE s3_application_policy
+(
+    id             character varying(26) NOT NULL PRIMARY KEY,
+    version        character varying(26) NOT NULL UNIQUE,
+    application_id character varying(26),
+    is_active      boolean               NOT NULL,
+    created_at     timestamp             NOT NULL,
+    updated_at     timestamp             NOT NULL,
+    kind           character varying(32) CHECK ( value IN ('file_extensions', 'rate_limit')),
+    -- examples:
+    --      * file extension: pdf txt zip gz
+    --      * rate limit: 1MB/user/hour
+    value          text
+);
+
+CREATE TABLE s3_upload_token
+(
+    id           character varying(26)  NOT NULL PRIMARY KEY,
+    namespace_id character varying(26)  NOT NULL,
+    user_id      character varying(26)  NOT NULL,
+    file_path    CHARACTER VARYING(128) NOT NULL,
+    created_at   timestamp              NOT NULL
+);
+
+-- TODO: Remove file schema of S3 object
+CREATE TABLE s3_file
+(
+    id             character varying(26)    NOT NULL PRIMARY KEY,
+    version        character varying(26)    NOT NULL UNIQUE,
+    application_id character varying(26)    NOT NULL,
+    size           float CHECK (value >= 0) NOT NULL, -- in byte
+    path           character varying(128)   NOT NULL,
+    is_active      boolean                  NOT NULL,
+    created_at     timestamp                NOT NULL,
+    updated_at     timestamp                NOT NULL
+);
