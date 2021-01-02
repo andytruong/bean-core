7ceb6d3e86c39b1c181230cd5fd6c279cfb9296f
Andy Truong <thehongtt@gmail.com>
2021-01-02 22:42:17 +1000

    config bundle > variable load: verify load key.

--- pkg/config/variable.service.go
+++ pkg/config/variable.service.go
@@ -3,9 +3,9 @@ package config
 import (
 	"context"
 	"time"
-
+	
 	"github.com/pkg/errors"
-
+	
 	"bean/components/claim"
 	"bean/components/connect"
 	"bean/components/scalar"
@@ -24,33 +24,36 @@ func (srv VariableService) access(ctx context.Context, bucketId string, action s
 	if nil != err {
 		return false, err
 	}
-
+	
 	if nil == bucket {
 		return false, nil
 	}
-
+	
 	claims := claim.ContextToPayload(ctx)
 	isOwner := (nil != claims) && claims.UserId() == bucket.HostId
 	isMember := (nil != claims) && claims.SpaceId() == bucket.HostId
-
+	
 	switch action {
 	case "read":
 		return bucket.Access.CanRead(isOwner, isMember), nil
-
+	
 	case "write":
 		return bucket.Access.CanWrite(isOwner, isMember), nil
-
+	
 	case "delete":
 		return bucket.Access.CanDelete(isOwner, isMember), nil
 	}
-
+	
 	return false, nil
 }
 
 func (srv VariableService) Load(ctx context.Context, key dto.VariableKey) (*model.ConfigVariable, error) {
+	if (key.Id == "") && (key.BucketId == "" || key.Name == "") {
+		return nil, errors.New("invalid load key")
+	}
+	
 	db := connect.ContextToDB(ctx)
 	variable := &model.ConfigVariable{}
-
 	if key.Id != "" {
 		err := db.Take(&variable, "id = ?", key.Id).Error
 		if nil != err {
@@ -62,13 +65,13 @@ func (srv VariableService) Load(ctx context.Context, key dto.VariableKey) (*mode
 			return nil, err
 		}
 	}
-
+	
 	if access, err := srv.access(ctx, variable.BucketId, "read"); nil != err {
 		return nil, err
 	} else if !access {
 		return nil, util.ErrorAccessDenied
 	}
-
+	
 	return variable, nil
 }
 
@@ -78,7 +81,7 @@ func (srv VariableService) Create(ctx context.Context, in dto.VariableCreateInpu
 	} else if !access {
 		return nil, util.ErrorAccessDenied
 	}
-
+	
 	bucket, err := srv.bundle.BucketService.Load(ctx, dto.BucketKey{Id: in.BucketId})
 	if nil != err {
 		return nil, err
@@ -92,10 +95,10 @@ func (srv VariableService) Create(ctx context.Context, in dto.VariableCreateInpu
 			err := util.NewError(util.ErrorCodeInput, []string{"VariableCreateInput.Value"}, reason)
 			errList = append(errList, err)
 		}
-
+		
 		return &dto.VariableMutationOutcome{Errors: errList}, nil
 	}
-
+	
 	variable := &model.ConfigVariable{
 		Id:          srv.bundle.idr.MustULID(),
 		Version:     srv.bundle.idr.MustULID(),
@@ -107,7 +110,7 @@ func (srv VariableService) Create(ctx context.Context, in dto.VariableCreateInpu
 		CreatedAt:   time.Now(),
 		UpdatedAt:   time.Now(),
 	}
-
+	
 	err = connect.ContextToDB(ctx).Create(&variable).Error
 	if nil != err {
 		return nil, err
@@ -122,45 +125,45 @@ func (srv VariableService) Update(ctx context.Context, in dto.VariableUpdateInpu
 	if nil != err {
 		return nil, err
 	}
-
+	
 	if access, err := srv.access(ctx, variable.BucketId, "write"); nil != err {
 		return nil, err
 	} else if !access {
 		return nil, util.ErrorAccessDenied
 	}
-
+	
 	if variable.Version != in.Version {
 		return nil, util.ErrorVersionConflict
 	} else {
 		changed := false
-
+		
 		if nil != in.Description {
 			if variable.Description != in.Description {
 				changed = true
 				variable.Description = in.Description
 			}
 		}
-
+		
 		if in.Value != nil {
 			if variable.Value != *in.Value {
 				changed = true
 				variable.Value = *in.Value
 			}
 		}
-
+		
 		if variable.IsLocked {
 			if changed {
 				return nil, util.ErrorLocked
 			}
 		}
-
+		
 		if nil != in.IsLocked {
 			if variable.IsLocked != *in.IsLocked {
 				changed = true
 				variable.IsLocked = *in.IsLocked
 			}
 		}
-
+		
 		if changed {
 			version := variable.Version
 			variable.Version = srv.bundle.idr.MustULID()
@@ -173,7 +176,7 @@ func (srv VariableService) Update(ctx context.Context, in dto.VariableUpdateInpu
 			}
 		}
 	}
-
+	
 	return &dto.VariableMutationOutcome{
 		Errors:   nil,
 		Variable: variable,
@@ -193,13 +196,13 @@ func (srv VariableService) Delete(ctx context.Context, in dto.VariableDeleteInpu
 		} else if !access {
 			return nil, util.ErrorAccessDenied
 		}
-
+		
 		err := tx.Delete(variable, "id = ?", variable.Id).Error
 		if nil != err {
 			return nil, err
 		}
 	}
-
+	
 	return &dto.VariableMutationOutcome{
 		Errors:   nil,
 		Variable: variable,
